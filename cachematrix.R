29fb1e1ed0aa673a99b06bb9862240a9643eae9b
Christian Schuhmann <chris@madebyherzblut.com>
2014-07-27 17:26:45 +0200

    Add my solution
    See the appended test case on how to use the function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This is my solution to the Programming Assignment 2 for the R Programming
+# course.
 
-## Write a short comment describing this function
-
+## This function creates a self-cacheable matrix to easily store the inversion
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+# This function checks if the matrix was inversed previously and return the
+# result from cache if possbile.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+
+    # Check if there is already a cached inversion. If so, simply return it
+    if (! is.null(m)) {
+      message("Returning matrix from cache.")
+      return(m)
+    }
+
+    message("Inverting matrix and persisting the result.")
+
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
+
+# Test Case:
+#> test <- makeCacheMatrix(matrix(c(-3, 2, 0, 2, -1, 0, 1, -2, 1), 3, 3))
+#> cacheSolve(test)
+#> cacheSolve(test)
